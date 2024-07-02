016c99f8797629acd529fe3d42a8c764f41660b6
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-07-02 16:37:26 +0530

    Added Github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,48 @@
 import Home from "./Components/Home";
 import Team from "./Components/Team"
-
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-export default function App(){
+export default function App() {
   return <>
-  <Router>
-   <div>
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://kare.acm.org/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://i.postimg.cc/GhcsnT4P/acm-logo.jpg" className="h-8" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">KARE-ACM</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li className="text-white"><Link to="/">Home</Link></li>
-      <li className="text-white"><Link to="/Team">Team</Link></li>
-      <li className="text-white"><Link to="/Events">Events</Link></li>
-      <li className="text-white"><Link to="/Gallery">Gallery</Link></li>
-      <li className="text-white"><Link to="/Blog">Blog</Link></li>
+    <Router>
+      <div>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://kare.acm.org/" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src="https://i.postimg.cc/GhcsnT4P/acm-logo.jpg" className="h-8" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">KARE-ACM</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li className="text-white"><Link to="/">Home</Link></li>
+                <li className="text-white"><Link to="/Team">Team</Link></li>
+                <li className="text-white"><Link to="/Events">Events</Link></li>
+                <li className="text-white"><Link to="/Gallery">Gallery</Link></li>
+                <li className="text-white"><Link to="/Blog">Blog</Link></li>
+                <li>
+                  <a href="https://github.com/TirumalareddySai/KARE-ACM-STUDENT-CHAPTER" target="_blank" rel="noopener noreferrer" className="inline-flex items-center text-white bg-black hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+                    <svg className="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd"/>
+                    </svg>
+                    Contribute on GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-      </ul>
-    </div>
-  </div>
-</nav>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/Team" element={<Team/>} />
-          </Routes>
- </div>
-   </Router>
-</>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/Team" element={<Team/>} />
+        </Routes>
+      </div>
+    </Router>
+  </>
 }
