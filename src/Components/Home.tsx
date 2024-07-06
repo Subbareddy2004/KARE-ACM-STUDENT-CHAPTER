eb8cadb391b676b748d186eed2a511c02250efcf
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-07-06 17:57:47 +0530

    Your commit message

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,19 @@
-export default function Home(){
-    return <>
-    <div>
-    <iframe src="//stream.crichd.vip/update/ten1.php" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>
-    </div>
+export default function Home() {
+  return (
+    <>
+      <div>
+        <iframe
+          src="//stream.crichd.vip/update/ten1.php"
+          width="100%"
+          height="500px"
+          marginHeight={0}
+          marginWidth={0}
+          scrolling="no"
+          frameBorder={0}
+          allowFullScreen
+          allow="encrypted-media"
+        ></iframe>
+      </div>
     </>
+  );
 }
